1eaa4d43b11cd9a77f8c7d214711a97614dac2a2
xevrac <xevrac@gmail.com>
2025-09-20 00:34:17 +1000

    Simplify entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-# Minimal entrypoint for Piqueserver
-
-cd /home/container || exit 1
-
-# Trap SIGINT and rethrow as SIGTERM for compatibility
-trap 'trap - SIGINT && kill -SIGTERM $$' SIGINT
-
-# Replace vars like {{SERVER_PORT}} and {{OPTIONS}}
-MODIFIED_STARTUP=$(echo -e "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
-
-echo "Starting Piqueserver with: ${MODIFIED_STARTUP}"
-
-# Run the server
-eval ${MODIFIED_STARTUP}
+exec python3 -m piqueserver
