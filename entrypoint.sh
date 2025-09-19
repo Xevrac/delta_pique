72e6816672b3135b35394512679c808894f08499
xevrac <xevrac@gmail.com>
2025-09-20 00:45:50 +1000

    Updates entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-exec python3 -m piqueserver
+# Entry point for Piqueserver in Pterodactyl
+
+# Export Pterodactyl variables
+export PORT=${SERVER_PORT}
+export OPTIONS="${OPTIONS}"
+
+# Make Python modules in /mnt/server visible
+export PYTHONPATH=/mnt/server:$PYTHONPATH
+
+# Change to server working directory
+cd /mnt/server || exit 1
+
+# Run Piqueserver in foreground
+python3.12 -m piqueserver
