f702dc36570a5e242c63f4a409ea56976c24378d
Rhys <xevrac@gmail.com>
2025-11-20 00:01:27 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # The `piqueserver` package was installed here using --target=/mnt/server
-export PYTHONPATH="/mnt/server:$PYTHONPATH"
+export PYTHONPATH="/home/container/:/mnt/server/:$PYTHONPATH"
 
 # Run piqueserver using the system's python3 binary.
 exec python3 -m piqueserver
